a685cdd7885a39b59cea80287f042281a24ff67f
EvertonLDS815 <evertonlino1234@gmail.com>
2023-08-01 18:40:33 -0300

    feat: add overlay and closeMenu

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
 const buttonMenu = document.querySelector(".bx-menu");
 const nav = document.querySelector("header nav");
+const overlay = document.querySelector("#overlay");
 
 function openMenu() {
     if(nav.style.width === "var(--width-menu)") {
-        closeMenu()
+        closeMenu();
     } else {
-        nav.style.width = "var(--width-menu)"
+        nav.style.width = "var(--width-menu)";
+        overlay.style.display = "block";
     }
 }
+buttonMenu.addEventListener("click", openMenu);
 
 function closeMenu() {
     nav.style.width = "var(--width-menu-initial)";
+    overlay.style.display = "none";
 }
-buttonMenu.addEventListener("click", openMenu)
+overlay.addEventListener("click", closeMenu)
