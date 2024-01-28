a594a8e983ec9c2ac67bd4be28659fab83be88ef
Everton Lino <94932393+EvertonLDS815@users.noreply.github.com>
2024-01-28 17:25:21 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const buttonMenu = document.querySelector(".bx-menu");
 const nav = document.querySelector("header nav");
 const overlay = document.querySelector("#overlay");
+const myDate = document.getElementById("date");
 
 function openMenu() {
     if(nav.style.width === "var(--width-menu)") {
@@ -16,4 +17,8 @@ function closeMenu() {
     nav.style.width = "var(--width-menu-initial)";
     overlay.style.display = "none";
 }
-overlay.addEventListener("click", closeMenu)
+overlay.addEventListener("click", closeMenu);
+
+const newDate = new Date();
+const year = newDate.getFullYear();
+myDate.innerHTML = `© ${year} - Todos os Direitos Reservados`
